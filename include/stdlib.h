bbc258f1b43abf8d0efc53d4ba576fb96a8e92a7
Phillip Johnston <phillip@embeddedartistry.com>
2017-10-10 11:06:53 -0700

    Implement strtof Fixes phillipjohnston/embedded-artistry#45

--- include/stdlib.h
+++ include/stdlib.h
@@ -40,6 +40,7 @@ double atof(const char*);
 
 #pragma mark - str-to-x -
 
+float strtof(const char* __restrict, char** __restrict);
 double strtod(const char* __restrict, char** __restrict);
 long strtol(const char* __restrict, char** __restrict, int);
 unsigned long strtoul(const char* __restrict, char** __restrict, int);
