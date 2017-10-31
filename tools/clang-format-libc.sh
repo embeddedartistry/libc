8ce02f7d92655a763ac46be9fef94491b6072d67
Phillip Johnston <phillip@embeddedartistry.com>
2017-10-31 13:07:30 -0700

    Exclude gdtoa in format check

--- tools/clang-format-libc.sh
+++ tools/clang-format-libc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-find . -type d \( -path ./test/cmocka \) -prune \
+find . -type d \( -path ./test/cmocka -o -path ./src/gdtoa \) -prune \
 	-o -iname *.h -o -iname *.c -o -iname *.cpp -o -iname *.hpp \
 	| xargs clang-format -style=file -i -fallback-style=none
 
