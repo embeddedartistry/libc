dd3f43dfe60688e668cd020f427d9cf1a34f770a
Phillip Johnston <phillip@embeddedartistry.com>
2017-12-14 13:21:53 -0800

    Add example Doxygen documentation to cytype.h to get started

--- include/ctype.h
+++ include/ctype.h
@@ -5,22 +5,49 @@
 extern "C" {
 #endif
 
-int isalnum(int);
-int isalpha(int);
-int isascii(int);
-int isblank(int);
-int iscntrl(int);
-int isdigit(int);
-int isgraph(int);
-int islower(int);
-int isprint(int);
-int ispunct(int);
-int isspace(int);
-int isupper(int);
-int isxdigit(int);
-int tolower(int);
-int toupper(int);
-int toascii(int);
+/**
+* @brief Checks if the given character is an alphanumeric character
+*
+* Checks if the given character is an alphanumeric character using the default locale.
+*	The following characters are alphanumeric:
+*		- digits (0123456789)
+*		- uppercase letters (ABCDEFGHIJKLMNOPQRSTUVWXYZ)
+*		- lowercase letters (abcdefghijklmnopqrstuvwxyz)
+*
+*   The behavior is undefined if the value of ch is not representable as unsigned char and is not equal to EOF.
+*
+* @param ch The character to classify
+* @return Non-zero value if the character is an alphanumeric character, 0 otherwise.
+*/
+int isalnum(int ch);
+
+/**
+* @brief Checks if the given character is an alphabetic character
+*
+* Checks if the given character is an alphabetic character, i.e. either an uppercase letter
+*	(ABCDEFGHIJKLMNOPQRSTUVWXYZ), or a lowercase letter (abcdefghijklmnopqrstuvwxyz)
+*
+* The behavior is undefined if the value of ch is not representable as unsigned char and is not equal to EOF.
+*
+* @param ch The character to classify
+* @return Non-zero value if the character is an alphabetic character, zero otherwise.
+*/
+int isalpha(int ch);
+
+int isascii(int ch);
+int isblank(int ch);
+int iscntrl(int ch);
+int isdigit(int ch);
+int isgraph(int ch);
+int islower(int ch);
+int isprint(int ch);
+int ispunct(int ch);
+int isspace(int ch);
+int isupper(int ch);
+int isxdigit(int ch);
+int tolower(int ch);
+int toupper(int ch);
+int toascii(int ch);
 
 #ifdef __cplusplus
 }
