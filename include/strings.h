6c4a6a36cdaf9eda52f6d65dfd9ed59ffdce5ff1
Seek Addo <dennis_addo@aol.com>
2017-12-17 20:18:33 +0100

    Doxygen Documentation: strings.h

--- include/strings.h
+++ include/strings.h
@@ -8,8 +8,40 @@
 extern "C" {
 #endif //__cplusplus
 
+/**
+* @brief Finds the last (most significant) bit set in the given mask
+*
+* Finds the last (most significant) bit set in the given (int) mask and return the index of that bit.
+*
+* Bits are numbered starting at 1, the least significant bit.
+*
+* @param mask The bit mask
+* @return The index of the bit if mask is not zero, 0 otherwise.
+* */
 int fls(int mask);
+
+/**
+ * @brief Finds the last (most significant) bit set in the given mask
+ *
+ * Finds the last (most significant) bit set in the given (long) mask and return the index of that bit.
+ *
+ * Bits are numbered starting at 1, the least significant bit.
+ *
+ * @param mask The bit mask
+ * @return The index of the bit if mask is not zero, 0 otherwise.
+ * */
 int flsl(long mask);
+
+/**
+ * @brief Finds the last (most significant) bit set in the given mask
+ *
+ * Finds the last (most significant) bit set in the given (long long) mask and return the index of that bit.
+ *
+ * Bits are numbered starting at 1, the least significant bit.
+ *
+ * @param mask The bit mask
+ * @return The index of the bit if mask is not zero, 0 otherwise.
+ * */
 int flsll(long long mask);
 
 #ifdef __cplusplus
