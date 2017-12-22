8f0531d882bd526ca78a6030f449e0e3a8c75365
Seek Addo <dennis_addo@aol.com>
2017-12-22 20:15:23 +0100

    Doxygen Documentation: stdbool.h

--- include/stdbool.h
+++ include/stdbool.h
@@ -3,12 +3,17 @@
 
 #ifndef __cplusplus
 
-/**
-* Only define these for C, since C++ has its own bool support
-*/
+/*
+ * Only defined for C, since C++ has its own bool support
+ * */
 
+/** Expands to the integer constant 1. */
 #define true 1
+
+/** Expands to the integer constant 0. */
 #define false 0
+
+/** Expands to _Bool. */
 typedef _Bool bool;
 
 #endif
