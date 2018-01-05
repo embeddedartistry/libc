7e6e431783a9f453f30cea4884ef9fb469de2069
Seek Addo <dennis_addo@aol.com>
2018-01-05 17:02:27 +0100

    Doxygen Documentation: stddef.h Fixes #24

--- include/stddef.h
+++ include/stddef.h
@@ -7,17 +7,23 @@
 #pragma mark - NULL -
 
 #ifdef __cplusplus
+/*! Expands to 0L (for C++). Implementation-defined null pointer constant  */
 #define NULL 0L
 #else
-// not C++
+
+/*! Expands to ((void*)0) (not C++). Implementation-defined null pointer constant  */
 #define NULL ((void*)0)
 #endif
 
 #pragma mark - offsetof -
 
 #if __GNUC__ > 3
+
+/*! byte offset from the beginning of a struct type to specified member  */
 #define offsetof(type, member) __builtin_offsetof(type, member)
 #else
+
+/*! byte offset from the beginning of a struct type to specified member  */
 #define offsetof(type, member) ((size_t)((char*)&(((type*)0)->member) - (char*)0))
 #endif
 
@@ -25,6 +31,8 @@
 
 #if(defined(__STDC_VERSION__) && __STDC_VERSION__ >= 201112L) || \
 	(defined(__cplusplus) && __cplusplus >= 201103L)
+
+/*! A type with alignment requirement as great as any other scalar type  */
 typedef long double max_align_t;
 #endif
 
