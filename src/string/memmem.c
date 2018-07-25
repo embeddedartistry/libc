4fbf1cc008fc58b0006b298e96b95b191c3fe128
Phillip Johnston <phillip@embeddedartistry.com>
2018-07-25 08:46:15 -0700

    Add brackets to memmem.c implementation

--- src/string/memmem.c
+++ src/string/memmem.c
@@ -31,7 +31,6 @@
 /*
  * Find the first occurrence of the byte string s in byte string l.
  */
-
 void* memmem(const void* l, size_t l_len, const void* s, size_t s_len)
 {
 	register char *cur, *last;
@@ -40,22 +39,32 @@ void* memmem(const void* l, size_t l_len, const void* s, size_t s_len)
 
 	/* we need something to compare */
 	if(l_len == 0 || s_len == 0)
+	{
 		return NULL;
+	}
 
 	/* "s" must be smaller or equal to "l" */
 	if(l_len < s_len)
+	{
 		return NULL;
+	}
 
 	/* special case where s_len == 1 */
 	if(s_len == 1)
+	{
 		return memchr(l, (int)*cs, l_len);
+	}
 
 	/* the last position where its possible to find "s" in "l" */
 	last = (char*)cl + l_len - s_len;
 
 	for(cur = (char*)cl; cur <= last; cur++)
+	{
 		if(cur[0] == cs[0] && memcmp(cur, cs, s_len) == 0)
+		{
 			return cur;
+		}
+	}
 
 	return NULL;
 }
