116a882e21f4c4e416543f54b8700e7af166b1fd
Phillip Johnston <phillip@embeddedartistry.com>
2018-07-31 17:46:58 -0700

    First-pass STDIO implementation. Just uses printf.h definitions.
    For host builds, we rely on -lsystem to supply us with printf functionality

---
+++ include/stdarg.h
@@ -0,0 +1,20 @@
+#ifndef _STDARG_H
+#define _STDARG_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define va_start(v,l)   __builtin_va_start(v,l)
+#define va_end(v)       __builtin_va_end(v)
+#define va_arg(v,l)     __builtin_va_arg(v,l)
+#define va_copy(d,s)    __builtin_va_copy(d,s)
+
+typedef __builtin_va_list va_list;
+typedef __builtin_va_list __isoc_va_list;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ include/stdio.h
@@ -0,0 +1,9 @@
+#ifndef	STDIO_H__
+#define	STDIO_H__
+
+#undef EOF
+#define EOF (-1)
+
+#include "printf.h"
+
+#endif // STDIO_H__
