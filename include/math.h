41458b6089f5140d6336869624b4b437c8f195df
Phillip Johnston <phillip@embeddedartistry.com>
2018-08-01 12:33:25 -0700

    Add first-pass math.h support for fabs, fabsf (used to fix warnings in gdtoa)

---
+++ include/math.h
@@ -0,0 +1,16 @@
+#ifndef MATH_H__
+#define MATH_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+double      fabs(double);
+float       fabsf(float);
+//long double fabsl(long double);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //MATH_H__
