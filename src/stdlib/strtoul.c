dc2d2c722e6249ec5b8d4483a1c9a77293cf14b5
Phillip Johnston <phillip@embeddedartistry.com>
2018-08-02 08:08:43 -0700

    Split strtol, stroul and fix warnings

--- src/stdlib/strtoul.c
+++ src/stdlib/strtoul.c
@@ -70,12 +70,12 @@
 #include <stdlib.h>
 
 /*
- * Convert a string to a long integer.
+ * Convert a string to an unsigned long integer.
  *
  * Ignores `locale' stuff.  Assumes that the upper and lower case
  * alphabets and digits are each contiguous.
  */
-long strtol(nptr, endptr, base) const char* nptr;
+unsigned long strtoul(nptr, endptr, base) const char* nptr;
 char** endptr;
 register int base;
 {
@@ -85,23 +85,8 @@ register int base;
 	register unsigned long cutoff;
 	register int neg = 0, any, cutlim;
 
-	// check base
-	if(base < 0 || base > 36)
-	{
-		// errno = EINVAL
-
-		if(endptr)
-		{
-			*endptr = (char*)(uintptr_t)nptr;
-		}
-
-		return 0;
-	}
-
 	/*
-	 * Skip white space and pick up leading +/- sign if any.
-	 * If base is 0, allow 0x for hex and 0 for octal, else
-	 * assume decimal; if base is already 16, allow 0x.
+	 * See strtol for comments as to the logic used.
 	 */
 	do
 	{
@@ -113,7 +98,9 @@ register int base;
 		c = *s++;
 	}
 	else if(c == '+')
+	{
 		c = *s++;
+	}
 	if((base == 0 || base == 16) && c == '0' && (*s == 'x' || *s == 'X'))
 	{
 		c = s[1];
@@ -128,27 +115,8 @@ register int base;
 	}
 	if(base == 0)
 		base = c == '0' ? 8 : 10;
-
-	/*
-	 * Compute the cutoff value between legal numbers and illegal
-	 * numbers.  That is the largest legal value, divided by the
-	 * base.  An input number that is greater than this value, if
-	 * followed by a legal input character, is too big.  One that
-	 * is equal to this value may be valid or not; the limit
-	 * between valid and invalid numbers is then based on the last
-	 * digit.  For instance, if the range for longs is
-	 * [-2147483648..2147483647] and the input base is 10,
-	 * cutoff will be set to 214748364 and cutlim to either
-	 * 7 (neg==0) or 8 (neg==1), meaning that if we have accumulated
-	 * a value > 214748364, or equal but the next digit is > 7 (or 8),
-	 * the number is too big, and we will return a range error.
-	 *
-	 * Set any if any `digits' consumed; make it negative to indicate
-	 * overflow.
-	 */
-	cutoff = neg ? -(unsigned long)LONG_MIN : LONG_MAX;
-	cutlim = (int)(cutoff % (unsigned long)base);
-	cutoff /= (unsigned long)base;
+	cutoff = (unsigned long)ULONG_MAX / (unsigned long)base;
+	cutlim = (int)((unsigned long)ULONG_MAX % (unsigned long)base);
 	for(acc = 0, any = 0;; c = *s++)
 	{
 		if(isdigit(c))
@@ -180,7 +148,7 @@ register int base;
 	}
 	if(any < 0)
 	{
-		acc = neg ? (unsigned long)LONG_MIN : (unsigned long)LONG_MAX;
+		acc = ULONG_MAX;
 		//		errno = ERANGE;
 	}
 	else if(neg)
@@ -191,5 +159,5 @@ register int base;
 	{
 		*endptr = (char*)(uintptr_t)(any ? s - 1 : nptr);
 	}
-	return (long)(acc);
+	return (acc);
 }
