f00c8843173808f7a9073915d9f4446808e924f6
Phillip Johnston <phillip@embeddedartistry.com>
2018-08-03 16:36:06 -0700

    Format arch/ code

--- arch/aarch64/src/__aeabi_atexit.c
+++ arch/aarch64/src/__aeabi_atexit.c
@@ -1,6 +1,6 @@
-int __cxa_atexit(void (*func)(void *), void *arg, void *dso);
+int __cxa_atexit(void (*func)(void*), void* arg, void* dso);
 
-int __aeabi_atexit (void *obj, void (*func) (void *), void *d)
+int __aeabi_atexit(void* obj, void (*func)(void*), void* d)
 {
-	return __cxa_atexit (func, obj, d);
+	return __cxa_atexit(func, obj, d);
 }
