48a8a442e1a16230281cce37abd46f162f42c2f0
Phillip Johnston <phillip@embeddedartistry.com>
2018-09-28 11:48:44 -0700

    Update clang-format scripts to ignore printf and openlibm

--- tools/clang-format-libc.sh
+++ tools/clang-format-libc.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-find . -type d \( -path ./test/cmocka -o -path ./src/gdtoa \) -prune \
+find . -type d \( -path ./test/cmocka -o -path ./src/gdtoa \
+	-o -path ./printf -o -path ./openlibm \) -prune \
 	-o -iname *.h -o -iname *.c -o -iname *.cpp -o -iname *.hpp \
 	| xargs clang-format -style=file -i -fallback-style=none
 
