9a5d8ac363dfd02f13b7743a04ada5b5b9fbc375
Phillip Johnston <phillip@embeddedartistry.com>
2018-12-04 16:20:27 -0800

    Exclude external projects from ccc check

--- tools/complexity_check.sh
+++ tools/complexity_check.sh
@@ -6,6 +6,6 @@ MESON_SOURCE_ROOT=${MESON_SOURCE_ROOT:-./}
 
 cd "${MESON_SOURCE_ROOT}"
 
-lizard -x "./buildresults/*" -x "./src/gdtoa/*" \
+lizard -x "./buildresults/*" -x "./src/gdtoa/*" -x "./openlibm/*" -x "./printf/*" \
 	--length 75 --CCN 10 --arguments 10 \
 	"$@"
