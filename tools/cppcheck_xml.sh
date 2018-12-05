9110fda21d7df1bb3fa34b464c2a43099f1b9593
Phillip Johnston <phillip@embeddedartistry.com>
2018-12-04 16:25:44 -0800

    Exclude test files from cppcheck
    cppcheck is reporting use of invalid values in our tests, which we dont' want to remove.

--- tools/cppcheck_xml.sh
+++ tools/cppcheck_xml.sh
@@ -5,5 +5,5 @@ MESON_BUILD_ROOT=${MESON_BUILD_ROOT:-buildresults}
 
 cppcheck --quiet --enable=style --force --xml --xml-version=2 \
 	-I ${MESON_CURRENT_SOURCE_DIR}/include \
-	src test \
+	src \
 	2>${MESON_BUILD_ROOT}/cppcheck.xml
