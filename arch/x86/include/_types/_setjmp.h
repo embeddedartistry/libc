adc33067ec80a859bddefbe260e5ddef9c4b8a29
Phillip Johnston <phillip@embeddedartistry.com>
2019-03-08 11:48:20 -0800

    Add setjmp/longjmp files
    Files pulled from musl. Currently not used.

---
+++ arch/x86/include/_types/_setjmp.h
@@ -0,0 +1,6 @@
+#ifndef __SETJMP_DEF_H_
+#define __SETJMP_DEF_H_
+
+typedef unsigned long long __jmp_buf[8];
+
+#endif // __SETJMP_DEF_H_
---
+++ include/setjmp.h
@@ -0,0 +1,31 @@
+#ifndef __SETJMP_H_
+#define __SETJMP_H_
+
+#include <_types/_setjmp.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct __jmp_buf_tag
+{
+	__jmp_buf __jb;
+	unsigned long __fl;
+	unsigned long __ss[128 / sizeof(long)];
+} jmp_buf[1];
+
+typedef jmp_buf sigjmp_buf;
+int sigsetjmp(sigjmp_buf, int);
+void siglongjmp(sigjmp_buf, int) __attribute__((noreturn));
+
+int _setjmp(jmp_buf);
+int setjmp(jmp_buf);
+
+void _longjmp(jmp_buf, int) __attribute__((noreturn));
+void longjmp(jmp_buf, int) __attribute__((noreturn));
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __SETJMP_H_
