81958b8068f173db483cf198a7391b358de50f56
Phillip Johnston <phillip@embeddedartistry.com>
2019-07-16 10:31:50 -0700

    Well, we have no consensus on whether int64_t and uint64_t should be long or long long on a 64-bit platform
    And this is causing us problems with system definition conflicts if we want to do something like include pthread.

--- arch/x86_64/include/_types/_int64_t.h
+++ arch/x86_64/include/_types/_int64_t.h
@@ -1,6 +1,10 @@
 #ifndef INT64_T_H_
 #define INT64_T_H_
 
+#ifdef __APPLE__
 typedef long long int64_t;
+#else
+typedef long int64_t;
+#endif
 
 #endif // __INT64_T_H_
--- arch/x86_64/include/_types/_uint64_t.h
+++ arch/x86_64/include/_types/_uint64_t.h
@@ -1,6 +1,10 @@
 #ifndef UINT64_T_H_
 #define UINT64_T_H_
 
+#ifdef __APPLE__
 typedef unsigned long long uint64_t;
+#else
+typedef unsigned long uint64_t;
+#endif
 
 #endif //__UINT64_T_H_
