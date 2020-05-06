0e0c739f3a122257b2aa38aa36ef656d12e477bd
Phillip Johnston <phillip@embeddedartistry.com>
2020-05-06 14:40:58 -0700

    Remove old scripts and Doxyfile in preparation for latest build system changes

--- tools/clang-format-libc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-find . -type d \( -path ./test/cmocka -o -path ./src/gdtoa \
-	-o -path ./printf -o -path ./openlibm \) -prune \
-	-o -iname *.h -o -iname *.c -o -iname *.cpp -o -iname *.hpp \
-	| xargs clang-format -style=file -i -fallback-style=none
-
-exit 0
--- tools/clear_test_xml.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-if [ -d "${MESON_BUILD_ROOT}/test" ]
-then
-	rm -f ${MESON_BUILD_ROOT}/test/*.xml
-fi
