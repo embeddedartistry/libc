21966789530eab96a733308d1634ad274fa4a8a4
Phillip Johnston <phillip@embeddedartistry.com>
2020-05-06 09:14:26 -0700

    Adjust code for wcint type change

--- src/wchar/towupper.c
+++ src/wchar/towupper.c
@@ -3,5 +3,5 @@
 
 wint_t towupper(wint_t wc)
 {
-	return (wint_t)(wc < 128 ? toupper((int)wc) : __towcase((wchar_t)wc, 0));
+	return (wint_t)(wc < 128 ? (wint_t)toupper((int)wc) : __towcase((wchar_t)wc, 0));
 }
