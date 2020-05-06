cbf30022d44a087cc2965d6bb4015b4a235eafa9
Phillip Johnston <phillip@embeddedartistry.com>
2020-05-06 09:12:39 -0700

    Add test code to cppcheck

--- tools/cppcheck_xml.sh
+++ tools/cppcheck_xml.sh
@@ -5,5 +5,5 @@ MESON_BUILD_ROOT=${MESON_BUILD_ROOT:-buildresults}
 
 cppcheck --quiet --enable=style --force --xml --xml-version=2 \
 	-I ${MESON_CURRENT_SOURCE_DIR}/include \
-	src \
+	src test \
 	2>${MESON_BUILD_ROOT}/cppcheck.xml
