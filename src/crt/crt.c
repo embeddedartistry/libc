90a27cb603d0195cc7690c6d8bf75a0f9395d6af
Phillip Johnston <phillip@embeddedartistry.com>
2020-10-14 14:14:36 -0700

    Address pointer math bug in crt.c
    Originally we used void* for calculations, but this is a GCC extension. I switched to uintptr_t, but then I was doing actual math. This meant that the startup code was calculating values incorrectly, and running 4x the actual functions that were supposed to be run.
    
    For portability reasons, we keep the uintptr math, and then we convert back to the actual count of functions using shift operations.

--- src/crt/crt.c
+++ src/crt/crt.c
@@ -5,12 +5,12 @@
 
 extern int __bss_start__;
 extern int __bss_end__;
-extern void (*__preinit_array_start[])(void) __attribute__((weak));
-extern void (*__preinit_array_end[])(void) __attribute__((weak));
-extern void (*__init_array_start[])(void) __attribute__((weak));
-extern void (*__init_array_end[])(void) __attribute__((weak));
-extern void (*__fini_array_start[])(void) __attribute__((weak));
-extern void (*__fini_array_end[])(void) __attribute__((weak));
+extern void (**__preinit_array_start)(void) __attribute__((weak));
+extern void (**__preinit_array_end)(void) __attribute__((weak));
+extern void (**__init_array_start)(void) __attribute__((weak));
+extern void (**__init_array_end)(void) __attribute__((weak));
+extern void (**__fini_array_start)(void) __attribute__((weak));
+extern void (**__fini_array_end)(void) __attribute__((weak));
 extern int main();
 
 __attribute__((weak)) int entry(void)
@@ -19,16 +19,38 @@ __attribute__((weak)) int entry(void)
 	return main();
 }
 
+/** Pointer Count Decimator
+ *
+ * We can't do math on void*, and we want to have something that works
+ * for 16-bit, 32-bit, and 64-bit systems. So we will be calculating "counts"
+ * based on the raw bytes, and converting that value into the number of "words"
+ * or "registers" or "pointers" that will be loaded into the arrays for processing.
+ *
+ * We'll define a shift offset that can be used to convert the values, rather
+ * than doing a straight division.
+ */
+#if UINTPTR_MAX == 0xFFFFu
+#define COUNT_TO_PTR_ADJUSTMENT 1U
+#elif UINTPTR_MAX == 0xFFFFFFFFu
+#define COUNT_TO_PTR_ADJUSTMENT 2U
+#elif UINTPTR_MAX == 0xFFFFFFFFFFFFFFFFu
+#define COUNT_TO_PTR_ADJUSTMENT 3U
+#else
+#error Processor pointer size is unsupported!
+#endif
+
 // This function may call another function which changes __stack_chk_guard
 __attribute__((no_stack_protector)) void __libc_init_array(void)
 {
-	size_t count = (size_t)((uintptr_t)__preinit_array_end - (uintptr_t)__preinit_array_start);
+	size_t count = (size_t)(((uintptr_t)__preinit_array_end - (uintptr_t)__preinit_array_start) >>
+							COUNT_TO_PTR_ADJUSTMENT);
 	for(size_t i = 0; i < count; i++)
 	{
 		__preinit_array_start[i]();
 	}
 
-	count = (size_t)((uintptr_t)__init_array_end - (uintptr_t)__init_array_start);
+	count = (size_t)(((uintptr_t)__init_array_end - (uintptr_t)__init_array_start) >>
+					 COUNT_TO_PTR_ADJUSTMENT);
 	for(size_t i = 0; i < count; i++)
 	{
 		__init_array_start[i]();
@@ -37,7 +59,8 @@ __attribute__((no_stack_protector)) void __libc_init_array(void)
 
 void __libc_fini_array(void)
 {
-	size_t count = (size_t)((uintptr_t)__fini_array_end - (uintptr_t)__fini_array_start);
+	size_t count = (size_t)(((uintptr_t)__fini_array_end - (uintptr_t)__fini_array_start) >>
+							COUNT_TO_PTR_ADJUSTMENT);
 	for(size_t i = count; i > 0; i--)
 	{
 		__fini_array_start[i - 1]();
