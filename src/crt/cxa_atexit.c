af62b20e222edc5b55013488123bedfe30d6c9c4
Phillip Johnston <phillip@embeddedartistry.com>
2020-10-18 11:05:49 -0700

    Add basic definition for __dso_handle to __cxa_atexit

--- src/crt/cxa_atexit.c
+++ src/crt/cxa_atexit.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 
-__attribute__((weak)) int cxa_atexit(void (*function)(void*), void* arg, void* d)
+__attribute__((weak)) int __cxa_atexit(void (*function)(void*), void* arg, void* d)
 {
 	(void)function;
 	(void)arg;
@@ -9,3 +9,6 @@ __attribute__((weak)) int cxa_atexit(void (*function)(void*), void* arg, void* d
 	// EA libc does not exit on baremetal systems
 	return 0;
 }
+
+// We aren't using shared libraries, so __dso_handle can be set to NULL
+void* __dso_handle = NULL;
