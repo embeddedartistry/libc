dbc92cd3fa19afdf4a4521438f363ca546764886
Phillip Johnston <phillip@embeddedartistry.com>
2020-11-30 14:25:15 -0800

    Address missing declaration warning

--- src/crt/cxa_atexit.c
+++ src/crt/cxa_atexit.c
@@ -1,5 +1,7 @@
 #include <stdlib.h>
 
+int __cxa_atexit(void (*function)(void*), void* arg, void* dso);
+
 /// Embedded Artistry libc does not exit on baremetal systems, so this function does nothing.
 __attribute__((weak)) int __cxa_atexit(void (*function)(void*), void* arg, void* dso)
 {
