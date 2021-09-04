2c95f6e4224b097163eb263c14fab27e17c3aafc
Cel A. Skeggs <cela@mit.edu>
2021-09-04 18:35:29 -0400

    Fix endianness declarations for ARM platform
    The ARM platform previously referenced _LITTLE_ENDIAN and _BIG_ENDIAN
    instead of __LITTLE_ENDIAN and __BIG_ENDIAN (which are what is defined
    in endian.h and used for other platforms). This meant that ARM
    platforms were treated as big-endian regardless of actual endianness.
    By correcting the declarations to use the correct number of
    underscores, ARM platforms can now be correctly recognized as
    little-endian when __ARMEB__ is not defined.

--- arch/arm/include/_endian.h
+++ arch/arm/include/_endian.h
@@ -2,9 +2,9 @@
 #define __ARM_MACHINE_ENDIAN_H_
 
 #ifdef __ARMEB__
-#define __BYTE_ORDER _BIG_ENDIAN
+#define __BYTE_ORDER __BIG_ENDIAN
 #else
-#define __BYTE_ORDER _LITTLE_ENDIAN
+#define __BYTE_ORDER __LITTLE_ENDIAN
 #endif //__ARMEB__
 
 #endif //__ARM_MACHINE_ENDIAN_H_
